aad7cc31e26cfb8d40dc0897a5b9424e30fdc4c5
sharafian <ben@sharafian.me>
2018-09-21 17:54:00 -0700

    feat: make upload work

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Koa = require('koa')
 const port = process.env.PORT || 8088
 const router = require('koa-router')()
 const parser = require('koa-bodyparser')()
+const serve = require('koa-static')
+const path = require('path')
 const app = new Koa()
 
 const { Monetizer } = require('web-monetization-receiver')
@@ -28,7 +30,7 @@ router.get('/files/:name', async ctx => {
   }
 
   const stream = bucket.file(ctx.params.name).createReadStream()
-  ctx.body = ctx.webMonetization.monetizeStream(stream) 
+  ctx.body = ctx.webMonetization.monetizeStream(stream, {})
 })
 
 router.post('/files/:name', async ctx => {
@@ -37,7 +39,7 @@ router.post('/files/:name', async ctx => {
   }
   
   const stream = bucket.file(ctx.params.name).createWriteStream()
-  ctx.webMonetization.monetizeStream(ctx.req).pipe(stream)
+  ctx.webMonetization.monetizeStream(ctx.req, {}).pipe(stream)
 
   await new Promise(resolve => {
     ctx.req.on('end', resolve)
@@ -54,6 +56,7 @@ app
   .use(parser)
   .use(router.routes())
   .use(router.allowedMethods())
+  .use(serve(path.resolve(__dirname, 'static')))
   .listen(port)
 
 console.log('listening. port=' + port)
